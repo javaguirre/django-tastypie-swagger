8e546742c2c0a41c351a5a9408bed1852da5f060
Javier Aguirre <contacto@javaguirre.net>
2013-04-19 12:41:14 +0200

    Fix for the name of the resource, the accordion for calls information wasn't working

--- tastypie_swagger/static/tastypie_swagger/js/lib/swagger.js
+++ tastypie_swagger/static/tastypie_swagger/js/lib/swagger.js
@@ -191,8 +191,7 @@
       this.path = this.api.resourcePath != null ? this.api.resourcePath : resourceObj.path;
       this.description = resourceObj.description;
       parts = this.path.split("/");
-      // this.name = parts[parts.length - 1].replace('.{format}', '');
-      this.name = this.path.replace('/', '');
+      this.name = parts[parts.length - 2].replace('.{format}', '');
       this.basePath = this.api.basePath;
       this.operations = {};
       this.operationsArray = [];
